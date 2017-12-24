c63ba9891eaea80b2c0c7c5c785b4df4c9236393
Masanori Ohgita <mp@ohgita.info>
2017-12-24 14:24:48 +0900

    Add: TVSchedules - channelTypes filter on fetch method

--- libs/tv-schedules/index.js
+++ libs/tv-schedules/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-const Nightmare = require('nightmare');
+const TvScheduleItem = require('./tv-schedule-item');
 
 
 /**
@@ -23,8 +23,6 @@ const TvSchedules = function (client) {
 
 	this.client = client;
 
-	this.nightmare = Nightmare({});
-
 };
 
 
@@ -32,7 +30,7 @@ const TvSchedules = function (client) {
  * A method to fetch a list of the scheduled Tv programs<br>
  * 放送が予定されているテレビ番組のリストを取得するメソッド<br>
  * @param  {TvSchedules~fetchFilter} filter      A filter conditions
- * @param  {TvSchedules~fetchCalback} callback  An callback function
+ * @param  {TvSchedules~fetchCallback} callback  An callback function
  */
 TvSchedules.prototype.fetch = function (filter = {}, callback) {
 
@@ -40,28 +38,71 @@ TvSchedules.prototype.fetch = function (filter = {}, callback) {
 
 	if (!filter.keyword) return callback(new Error('filter.keyword is required'), null);
 
-	// Set the session data
-	self.client.sessionData.forEach((session_data, index) => {
-
-		session_data.url = self.client.BASE_URL;
-		self.nightmare.cookies.set(session_data);
-
-	});
+	if (filter.channelTypes && filter.channelTypes.length == 0) return callback(new Error('filter.channelTypes is empty'));
 
 	// Execute searching
-	self.nightmare
+	self.client._getNightmare()
 		// Load the top page
 		.goto(self.client.BASE_URL)
-		// Input the form
+		// Wait until the search form is loaded
+		.wait('#frwSwhDtlBtn')
+		// Take a screen shot
+		.screenshot('tmp.png')
+		// Expand the search form
+		.evaluate(() => {
+
+			// Click a button
+			document.querySelector('#frwSwhDtlBtn').click();
+
+		})
+		// Wait until the search form is expanded
+		.wait('#frwSwhInitBtn')
+		// Take a screen shot
+		.screenshot('tmp.png')
+		// Input the search form
 		.evaluate((filter) => {
 
 			// Input the field - Keyword
 			document.getElementById('frwSwhInp').value = filter.keyword;
 
+			// Check a checkbox - Channel Type
+			if (filter.channelTypes) {
+				document.querySelectorAll('.frwSetChInpArea').forEach((elem, index) => {
+
+					const checkbox_elem = elem.querySelector('input');
+
+					// Unfortunately, We could not set a conditions,
+					//  even if we change a state of checkbox with using checked property.
+					// Instead, we will unchecked it with using click event.
+					if (elem.innerText == '地デジ') {
+						if (checkbox_elem.checked && filter.channelTypes.indexOf('TE') == -1) {
+							checkbox_elem.click(); // Unchecked
+						}
+					} else if (elem.innerText == 'BSデジ') {
+						if (checkbox_elem.checked && filter.channelTypes.indexOf('BS') == -1) {
+							checkbox_elem.click(); // Unchecked
+						}
+					} else if (elem.innerText == 'CSデジ') {
+						if (checkbox_elem.checked && filter.channelTypes.indexOf('CS') == -1) {
+							checkbox_elem.click(); // Unchecked
+						}
+					} else {
+						if (checkbox_elem.checked && filter.channelTypes.indexOf(elem.innerText) == -1) {
+							checkbox_elem.click(); // Unchecked
+						}
+					}
+
+				});
+			}
+
 			// Submit the form
-			document.getElementById('frwSwhBtn').click();
+			window.setTimeout(() => {
+				document.getElementById('frwSwhBtn').click();
+			}, 500);
 
 		}, filter)
+		// Take a screen shot
+		.screenshot('tmp.png')
 		// Wait
 		.wait(1000)
 		// Take a screen shot
@@ -77,6 +118,13 @@ TvSchedules.prototype.fetch = function (filter = {}, callback) {
 				// Get a program title
 				const program_title = item_elem.querySelector('.pgmLinkTtl').innerText;
 				if (program_title == '') {
+					// To next item
+					return;
+				}
+
+				// Get a program url
+				const program_url = item_elem.querySelector('.pgmLinkTtl').href;
+				if (program_url == '') {
 					// To next item
 					return;
 				}
@@ -104,6 +152,7 @@ TvSchedules.prototype.fetch = function (filter = {}, callback) {
 				// Insert to array
 				items.push({
 					title: program_title,
+					url: program_url,
 					startDate: program_start_date,
 					endDate: program_end_date,
 					broadcasterName: program_broadcaster
@@ -114,10 +163,18 @@ TvSchedules.prototype.fetch = function (filter = {}, callback) {
 			return items;
 
 		})
-		.end()
-		.then((items) => {
+		.then((fetched_items) => {
 
-			return callback(null, items);
+			// Initialize an instance of TvScheduleItem from fetched item
+			var instances = [];
+			fetched_items.forEach((item) => {
+
+				instances.push(new TvScheduleItem(self.client, item));
+
+			});
+
+			// Call the callback
+			return callback(null, instances);
 
 		})
 		.catch((error) => {
@@ -137,17 +194,14 @@ module.exports = TvSchedules;
 /**
  * Filter conditions for fetch(...) method
  * @typedef {Object} TvSchedules~fetchFilter
- * @property {String} keyword  A keyword
+ * @property {String} keyword           A keyword
+ * @property {String[]} [channelTypes]  A channel type (multiple values are supported)<br>
+ *   Available values: 'TE' (地上デジタル), 'BS' (BSデジタル), 'CS' (CSデジタル)
  */
 
 /**
  * Callback of fetch(...) method
  * @callback TvSchedules~fetchCallback
  * @param {Error} error      An error object (If something happened)
- * @param {Object[]} items   An array of TV program schedules
- * @param {String} items[].title         A name of the program
- * @param {Date} items[].startDate       A scheduled start date of the program
- * @param {Date} items[].endDate         A scheduled end date of the program
- * @param {String} items[].description   A description text of the program
- * @param {Function} items[].record      A function for reserving recording
+ * @param {TvScheduleItem[]} items   An array of TV program schedules
  */
